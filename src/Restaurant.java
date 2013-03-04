dbe2e17262fdae62a769e44fc70a454e9264130b
Guillaume Gédéon <guillaume.gedeon@laposte.net>
2013-03-04 09:48:23 +0100

    Restaurant + AppareilElectrique
    Petit Remplissage des classes

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -5,6 +5,22 @@ public class Restaurant {
 	
 	/*Attributs*/
 	
+	private Cuisine cuisine;
+	private Forfait forfait;
+	
+	public Cuisine getCuisine() {
+		return cuisine;
+	}
+	public void setCuisine(Cuisine cuisine) {
+		this.cuisine = cuisine;
+	}
+	public Forfait getForfait() {
+		return forfait;
+	}
+	public void setForfait(Forfait forfait) {
+		this.forfait = forfait;
+	}
+	
 	
 
 }
