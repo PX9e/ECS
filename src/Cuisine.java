3656d2a7067400fdd0db8034a1f118aa82ac69f5
Lastecoueres Guillaume <PX9e@gmx.fr>
2013-03-04 10:19:03 +0100

    Getters and setters + Plan d'allumage

--- src/Cuisine.java
+++ src/Cuisine.java
@@ -6,4 +6,39 @@ public class Cuisine {
 	private ArrayList<AppareilElectrique> AppareilsElectriques;
 	
 	
+	public void AjouterAppareil(AppareilElectrique MonAppareil)
+	{
+		AppareilsElectriques.add(MonAppareil);
+	}
+	
+	
+	public AppareilElectrique ObtenirAppareil(int i)
+	{
+		if(i > AppareilsElectriques.size())
+		{
+			return AppareilsElectriques.get(i);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	public ArrayList<AppareilElectrique> ObtenirAppareils(int i)
+	{
+		return AppareilsElectriques;
+	}
+	
+	Cuisine()
+	{
+		AppareilsElectriques = new ArrayList<AppareilElectrique>();
+	}
+	
+	Cuisine( ArrayList<AppareilElectrique> MesAppareils)
+	{
+		this.AppareilsElectriques = MesAppareils;
+	}
+	
+	
+	
 }
